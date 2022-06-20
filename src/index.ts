1ca17132e47981193a1b8e5cc41c1dad80b4e80b
alaaqutb <you@example.com>
2022-06-20 06:41:24 +0200

    add sharp

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import express from 'express';
-//import sharp from 'sharp';
+import sharp from 'sharp';
+
+
+// const thumbDir = path.resolve(__dirname, './../images/thumb');
 const app = express();
 
 const port = 3000;
-//const _sharp =sharp();
+
 
 
  app.get('/api', (req, res) => {
-   res.send('Hello, world!');
- });
+   res.send('please get Image...');
+   const name=req.query.name;
+   const width:string=req.query.width as string ;
+   const height:string=req.query.height as string;
+   console.log(name,width,height);
 
-
+   const widthpars:number=parseInt(width);
+   const heightpars:number=parseInt(height);
+   console.log(widthpars);
+   console.log(heightpars);
+   
+   sharp(`./images/full/${name}.jpg`).resize(widthpars,heightpars).toFile(`./images/thumb/${name}_${widthpars}_${heightpars}.jpg`);
+//    app.use(express.static('public')); 
+//    app.use(`/images/thumb/${name}_${widthpars}_${heightpars}.jpg`, express.static('images'));
+}
+ );
  app.listen(port, ()=> {
   console.log(`server started at localhost:${port}`)
  });
